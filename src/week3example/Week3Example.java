de8e14f93e85b5a97e3f349247dd0a8f565dcbc2
Kaur6869 <59967437+Kaur6869@users.noreply.github.com>
2020-01-23 09:36:16 -0500

    Update Week3Example.java

--- src/week3example/Week3Example.java
+++ src/week3example/Week3Example.java
@@ -6,7 +6,10 @@ public class Week3Example {
 
     // date 23/01/2020
     public static void main(String[] args) {
-       System.out.println("This is week 3");
+    
+        System.out.println("This is week 3");
+         System.out.println("This is week 3 Fetch + Merge");
     }
+    
     
 }
