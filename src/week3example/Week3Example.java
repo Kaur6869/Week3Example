a32d4f79076f39f04ed895efb2b23caf012789d7
Kaur6869 <59967437+Kaur6869@users.noreply.github.com>
2020-01-23 09:32:47 -0500

    Update Week3Example.java

--- src/week3example/Week3Example.java
+++ src/week3example/Week3Example.java
@@ -4,7 +4,7 @@ package week3example;
 
 public class Week3Example {
 
-    
+    // date 23/01/2020
     public static void main(String[] args) {
        System.out.println("This is week 3");
     }
